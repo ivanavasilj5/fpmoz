fb78948293c4d3a5de5d35a4afd613789de88e2a
ivanavasilj5 <ivana.vasilj2@fpmoz.sum.ba>
2023-09-17 17:02:47 +0200

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,132 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    query: "",
+    filteredNews: [],
+    usersList: [
+      {
+        id: 1,
+        email: "dario90@gmail.com.com",
+        firstName: "Dario",
+        lastName: "Marincic",
+        office: "IT Company",
+        phone: "+63446648",
+      },
+      {
+        id: 2,
+        email: "marijatolj@hotmail.com",
+        firstName: "Marija",
+        lastName: "Tolj",
+        office: "Tech",
+        phone: "+63489300",
+      },
+      {
+        id: 3,
+        email: "klara@gmail.com",
+        firstName: "Klara",
+        lastName: "Marincic",
+        office: "IT Company",
+        phone: "+63831403",
+      },
+      {
+        id: 4,
+        email: "ivan95@gmail.com",
+        firstName: "Ivan",
+        lastName: "Vasilj",
+        office: "Tech",
+        phone: "+63910977",
+      },
+    ],
+  },
+  mutations: {
+    setNews(state, news) {
+      state.news = news;
+      state.filteredNews = news; // Initialize filtered news
+    },
+    filterNews(state, { query, startDate, endDate }) {
+      state.filteredNews = state?.news?.filter((newsItem) => {
+        const titleMatches = newsItem.title
+          .toLowerCase()
+          .includes(query.toLowerCase());
+
+        // Filter by date range
+        
+        const newsDate = new Date(newsItem.time * 1000);
+        const isWithinDateRange =
+          (!startDate || newsDate >= startDate) &&
+          (!endDate || newsDate <= endDate);
+
+        return titleMatches && isWithinDateRange;
+      });
+    },
+    deleteUser(state, id) {
+      const index = state.usersList
+        .map((x) => {
+          return x.id;
+        })
+        .indexOf(id);
+      state.usersList.splice(index, 1);
+    },
+    addUser(state, obj) {
+      const contact = {
+        id: Date.now(),
+        ...obj,
+      };
+      state.usersList.push(contact);
+    },
+
+    editUser(state, item) {
+      const items = state.usersList;
+      var foundIndex = items.findIndex((x) => x.id == item.id);
+      items[foundIndex] = item;
+    },
+  },
+  actions: {
+
+    async fetchNews({ commit }) {
+      try {
+        const response = await axios.get(
+          "https://hacker-news.firebaseio.com/v0/topstories.json"
+        );
+        const newsIds = response.data;
+    
+        // Fetch news items and filter based on date
+        const newsPromises = newsIds.map(async (id) => {
+          const itemResponse = await axios.get(
+            `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+          );
+          const itemData = itemResponse.data;
+               
+          // Filter by date (e.g., older than a week)
+          const oneWeekAgo = Date.now() - 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
+          if (itemData.time < oneWeekAgo) {
+            return itemData;
+          } else {
+            return null; // Ignore items that are not older than a week
+          }
+        });
+    
+        const newsData = await Promise.all(newsPromises);
+        const filteredNews = newsData?.filter((item) => item !== null); // Remove null entries
+    
+        commit("setNews", filteredNews);
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    },
+    
+  },
+  getters: {
+    userList: (state) => {
+      return state.usersList;
+    },
+
+    getFilteredNews(state) {
+      return state.filteredNews;
+    },
+  },
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,74 @@
+                    <template>
+  <v-container class="pt-16">
+    <v-row justify="space-around">
+      <v-card width="1200">
+        <v-img
+          height="200px"
+          contain
+          src="https://api.itkarijera.ba/api/cms/documentfile/download/1138"
+        >
+          <v-app-bar flat color="rgba(0, 0, 0, 0)">
+            <v-toolbar-title class="text-h6 indigo--text pl-0">
+              Opis rada
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn color="white" icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </v-app-bar>
+
+          <v-card-title class="white--text mt-16">
+            <v-avatar size="56">
+              <img
+                alt="logo"
+                src="https://w7.pngwing.com/pngs/178/551/png-transparent-vue-js-javascript-framework-front-and-back-ends-github-technical-angle-text-triangle.png"
+              />
+            </v-avatar>
+            <p class="ml-3 indigo--text">FPMOZ</p>
+          </v-card-title>
+        </v-img>
+
+        <v-card-text>
+          <div class="font-weight-bold ml-8 mb-2">Timeline projekta</div>
+
+          <v-timeline align-top dense>
+            <v-timeline-item
+              v-for="message in messages"
+              :key="message.time"
+              :color="message.color"
+              small
+            >
+              <div>
+                <div>{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    messages: [
+      {
+        message: ` U ovome projektu koristimo Hacker News up koji nam donosi vijesti. Vijesti mogu da se filtriraju po tipu i datumu.`,
+      },
+      {
+        message: `Imamo funkciju pretrazivanja kako bi nam olaksalo pregledavanje vijesti`,
+      },
+          {
+        message: ` Imamo CRUD operacije za korsnika, mogucnost dodavanja novog korisnika.
+        Takoder mozemo raditi izmjene korisnika i brisanje.`,
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+</style>
